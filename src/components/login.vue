9a2191ac335b6699e8b6bbb98bb284af43cde51a
vanuo <841805292@qq.com>
2019-03-30 11:37:52 +0800

    安装+路由预处理

---
+++ src/components/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+        登录
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+    .login {
+        width: 100px;
+        height: 100px;
+        background-color: pink;
+    }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入饿了么ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
+// 导入路由
+import router from './router.js'
 new Vue({
   render: h => h(App),
+  router //将路由挂载到vue实例上
 }).$mount('#app')
