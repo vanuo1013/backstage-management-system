5bd522542c39d5ad258ad23e5167170b10384285
vanuo <841805292@qq.com>
2019-04-02 13:25:09 +0800

    数据统计图表布局

--- src/router.js
+++ src/router.js
@@ -1,65 +1,74 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
 // 导入组件
-import login from './components/login.vue'
-import index from './components/index.vue'
-import users from './components/users.vue'
-import roles from './components/roles.vue'
-import rights from './components/rights.vue'
-import goods from './components/goods.vue'
-import params from './components/params.vue'
-import categories from './components/categories.vue'
-import orders from './components/orders.vue'
+import login from "./components/login.vue";
+import index from "./components/index.vue";
+import users from "./components/users.vue";
+import roles from "./components/roles.vue";
+import rights from "./components/rights.vue";
+import goods from "./components/goods.vue";
+import params from "./components/params.vue";
+import categories from "./components/categories.vue";
+import orders from "./components/orders.vue";
+import reports from "./components/reports.vue";
 
 // 规则
 let routes = [
-    {
-        path: "/login",
-        component: login
-    },
-    {
-        path: "/",
-        component: index,
-        children: [
-            {
-                path: "users",
-                component: users
-            },
-            {
-                path: "roles",
-                component: roles
-            },
-            {
-                path: "rights",
-                component: rights
-            },
-            {
-                path: "goods",
-                component: goods
-            },
-            {
-                path: "params",
-                component: params
-            },
-            {
-                path: "categories",
-                component: categories
-            },
-            {
-                path: "orders",
-                component: orders
-            },
-            
-        ]
-    }
-]
+  {
+    path: "/login",
+    component: login
+  },
+  {
+    path: "/",
+    component: index,
+    children: [
+      // 路由重定向, 嵌套路由中无需写/
+      {
+        path: "",
+        redirect: "users"
+      },
+      {
+        path: "users",
+        component: users
+      },
+      {
+        path: "roles",
+        component: roles
+      },
+      {
+        path: "rights",
+        component: rights
+      },
+      {
+        path: "goods",
+        component: goods
+      },
+      {
+        path: "params",
+        component: params
+      },
+      {
+        path: "categories",
+        component: categories
+      },
+      {
+        path: "orders",
+        component: orders
+      },
+      {
+        path: "reports",
+        component: reports
+      }
+    ]
+  }
+];
 
 // 实例化路由对象
 let router = new VueRouter({
-    routes
-})
+  routes
+});
 
 // 导出暴露
-export default router
+export default router;
